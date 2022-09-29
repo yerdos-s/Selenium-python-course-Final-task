86cc336ecf352763b7588dd036309ed58ea07f42
Yerdos Sadvakassov <erdos.sadvakasov@gmail.com>
2022-09-29 18:21:31 +0600

    webdriver_manager was added to conftest.py. Edge browser was added to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
@@ -15,13 +21,23 @@ def browser(request):
     driver = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
+        service = ChromeService(executable_path=ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(service=service,options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        driver = webdriver.Firefox()
+        service = FirefoxService(executable_path=GeckoDriverManager().install())
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        driver = webdriver.Firefox(service=service,firefox_profile=fp)
+    elif browser_name == "edge":
+        service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
+        options = webdriver.EdgeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        driver = webdriver.Edge(service=service,options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield driver
