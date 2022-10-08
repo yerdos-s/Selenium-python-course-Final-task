7f3fd7d76aee6b7bb8592324cc4bc810b283ac33
yslc <yerdossadvakassov@tengizchevroil.com>
2022-10-08 20:47:14 +0600

    deleted unnecessary comments from test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -7,10 +7,9 @@ from pages.basket_page import BasketPage
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self,browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser,
-                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        page = MainPage(browser,link)
+        page.open()
+        page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
